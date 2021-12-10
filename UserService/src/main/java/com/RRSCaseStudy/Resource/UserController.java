6eb9b1715a7804b37b1713e7f75ab8a3550b96d6
Humera Banu <humerashaik881@gmail.com>
2021-12-10 19:18:27 +0530

    interconnected userservice,with all user operation

--- UserService/src/main/java/com/RRSCaseStudy/Resource/UserController.java
+++ UserService/src/main/java/com/RRSCaseStudy/Resource/UserController.java
@@ -1,14 +1,27 @@
 package com.RRSCaseStudy.Resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.RRSCaseStudy.Model.Reservation;
 import com.RRSCaseStudy.Model.TrainAvailability;
+import com.RRSCaseStudy.Model.User;
+import com.RRSCaseStudy.Repository.UserRepository;
+
 
 
 
@@ -19,11 +32,51 @@ public class UserController {
 	public UserController(RestTemplateBuilder restTemplateBuilder) {
 		this.restTemplate=restTemplateBuilder.build();
 	}
+	
+//---------------User Operation On Service------------------------	
+	//------------------Viewing all the available Trains
 	public List<TrainAvailability> callForDisplaying()
 	{
 		return restTemplate.exchange("http://localhost:8082/train/ShowAllTrains",HttpMethod.GET,null,List.class).getBody();
 		
 		  //return restTemplate.getForObject("http://localhost:8002/train-availability-service/train/ShowAllTrains",List.class);	
 	}
-
+		
+	@RequestMapping("/{startLocation}")
+	public List<TrainAvailability> SeearchBySource(@PathVariable("startLocation") String startLocation)
+	{
+		return restTemplate.exchange("http://localhost:8082/train/findtrainBySource/"+startLocation,HttpMethod.GET,null,List.class).getBody();
+		//return restTemplate.getForObject("http://localhost:8002/train-availability-service/train/findtrainBySource/"+startLocation, List.class);
+	}
+	
+	//-------------Search by Destination location the train Details----------------------// 
+	@RequestMapping("/{destination}")
+	public List<TrainAvailability> SeearchByDestination(@PathVariable("destination") String destination)
+	{
+		return restTemplate.exchange("http://localhost:8082/train/findtrainByDestination/"+destination,HttpMethod.GET,null,List.class).getBody();
+		//return restTemplate.getForObject("http://localhost:8002/train-availability-service/train/findtrainByDestination/"+destination, List.class);
+	}
+	
+//---------------------user Operation on reservation----------------------
+	//--------------------Add Reservation--------------------
+	public String add(Reservation order)
+	{
+		HttpEntity<Reservation> entity = new HttpEntity<>(order);
+		return restTemplate.exchange("http://localhost:8083/reservation/addorder",HttpMethod.POST,entity,String.class).getBody();
+		
+	}
+	//-----------------View All Reservation----------------------
+	
+		public List<Reservation> getReservation()
+		{
+			return restTemplate.exchange("http://localhost:8083/reservation/getall",HttpMethod.GET,null,List.class).getBody();
+		}
+		
+		//------------------Delete Reservation---------------------
+		
+		@DeleteMapping("{/rId}")
+		public String deleteReservationByAdmin(@PathVariable int rId)
+		{
+			return restTemplate.exchange("http://localhost:8083/reservation/Delete/"+rId,HttpMethod.DELETE,null,String.class).getBody();
+		}
 }
--- UserService/src/main/java/com/RRSCaseStudy/Service/UserService.java
+++ UserService/src/main/java/com/RRSCaseStudy/Service/UserService.java
@@ -1,15 +1,25 @@
 package com.RRSCaseStudy.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.RRSCaseStudy.Model.Reservation;
 import com.RRSCaseStudy.Model.TrainAvailability;
+import com.RRSCaseStudy.Model.User;
+import com.RRSCaseStudy.Repository.UserRepository;
 import com.RRSCaseStudy.Resource.UserController;
+
 
 @RestController
 @RequestMapping("/users")
@@ -21,10 +31,89 @@ public class UserService {
 	public UserService(UserController user) {
 		this.user=user;
 	}
+	//------------------------User Operation on user Service------------
+		@Autowired
+		UserRepository userRepository;
+		@PostMapping("/AddUser")
+		public String AddTrain(@RequestBody User user)
+		{
+			userRepository.save(user);
+			return "User Details added with UserID "+ user.getUserId();
+		}
+		@GetMapping("/ShowAllUsers")
+		public List<User> getTrains()
+		{
+			return userRepository.findAll();
+		}
+		
+		@GetMapping("/findUserById/{userId}")
+		public Optional<User> getByID(@PathVariable int userId)
+		{
+			return userRepository.findById(userId);
+		}
+		
+		@GetMapping("/findUserByName/{userName}")
+		public Optional<User> getByName(@PathVariable String userName)
+		{
+			return userRepository.findByUserName(userName);
+		}
+		
+		@PutMapping("/updateUserdetails")
+		public String UpdateTrain(@RequestBody User user)
+		{
+			userRepository.save(user);
+			return "User Details updated with UserID "+ user.getUserId();
+		}
+		
+		@DeleteMapping("/delete/{userId}")
+		public String deleteTrainByID(@PathVariable int userId)
+		{
+			userRepository.deleteById(userId);
+			return "User info deleted with userid "+ userId;
+		}
+		
+//---------------------User Operation On Train Service---------------------------	
 	//-------------Display all the train Details----------------------// 
 		@GetMapping("/")
 		public List<TrainAvailability> consume1()
 		{
 			return user.callForDisplaying();
+		}
+		
+		//-------------Search by Source location the train Details----------------------// 
+		@GetMapping("/searchBySource1/{startLocation}")
+		public List<TrainAvailability> searchby(@PathVariable ("startLocation") String startLocation)
+		{
+			return user.SeearchBySource(startLocation);
+		}
+		
+		//-------------Search by Destination location the train Details----------------------// 
+		@GetMapping("/searchByDetination/{destination}")
+		public List<TrainAvailability> searchByDestination(@PathVariable ("destination") String destination)
+		{
+			 return user.SeearchByDestination(destination);
+			//return new ResponseEntity<>(admin.SeearchByDestination(destination),HttpStatus.OK);
+		}
+		
+//------------------------User operation On Reservation Service-----------
+		//-----------------Adding Reservation--------------
+		@PostMapping("/Add") 
+		public String add(@RequestBody Reservation order)
+		{
+			  return user.add(order);
+		}
+		//-------------------Displaying all Reservation--------
+		@GetMapping("/getreservation")
+		public List<Reservation> getOrder()
+		{
+			return user.getReservation();
+		}
+		
+
+		//---------------------------Delete Reservation-------------------------
+		@DeleteMapping("/deleteReservation/{rId}")
+		public String deleteReservation(@PathVariable int rId)
+		{
+		  return user.deleteReservationByAdmin(rId);
 		}
 }
